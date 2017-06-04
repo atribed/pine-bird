52b50218f7db4361d07b51efda9cdb4c595983bb
Arthur Gorka <azgorka@gmail.com>
2017-06-04 17:13:35 -0400

    Add initial sql migration and insert some initial data

---
+++ migrations/init_migration.sql
@@ -0,0 +1,57 @@
+CREATE TABLE bird (
+  id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(240),
+  scientific_name VARCHAR(240),
+  description TEXT,
+  length FLOAT(3,1),
+  wing_span FLOAT(3,1),
+  weight FLOAT(3,2),
+  migrates TINYINT(1),
+  img VARCHAR(400) DEFAULT NULL
+);
+
+CREATE TABLE descriptor_type (
+  id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(100)
+);
+
+CREATE TABLE descriptor (
+  id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+);
+
+CREATE TABLE bird_x_descriptor (
+  id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  bird_id INT(6) UNSIGNED,
+  descriptor_id INT(6) UNSIGNED,
+  descriptor_type_id INT(6) UNSIGNED
+);
+
+CREATE TABLE season (
+  id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(40)
+);
+
+CREATE TABLE bird_x_season (
+  id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  bird_id INT(6) UNSIGNED,
+  season_id INT(6) UNSIGNED
+);
+
+INSERT INTO descriptor_type (
+  id,
+  name
+) VALUES (
+  (1, color),
+  (2, call),
+  (3, size)
+);
+
+INSERT INTO season (
+  id,
+  name
+) VALUES (
+  (1, 'winter'),
+  (2, 'spring'),
+  (3, 'summer'),
+  (4, 'fall')
+);
