2897de5b761830671b53d8ca14c5baeac4ac26ef
Arthur Gorka <azgorka@gmail.com>
2017-06-11 13:32:14 -0400

    Remove fetch polyfill

--- js/app.js
+++ js/app.js
@@ -2,9 +2,6 @@ var React     = require('react');
 var ReactDOM  = require('react-dom');
 var Container = require('./components/container');
 
-// Fetch polyfill
-require('whatwg-fetch');
-
 var containerDiv = document.getElementById('pine-birds');
 
 if (containerDiv) {
